831b3f4c10eebf230cc1b000cfe3fe7fce05eb5b
Adam Mortka <a.mortka@gmail.com>
2017-02-12 22:12:40 +0100

    browser sync + sass + js

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,66 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
+/*var autoprefixer = require('gulp-autoprefixer');
+var csso         = require('gulp-csso');
+var debug        = require('gulp-debug');
+var filter       = require('gulp-filter');
+var jshint       = require('gulp-jshint');
+var sass         = require('gulp-sass');
+var uglify       = require('gulp-uglify');
+var useref       = require('gulp-useref');*/
 
-gulp.task('js', function () {
-    return gulp.src('./src/**/*.js')
-        .pipe(gulp.dest('./dist/'));
+var browserSync  = require('browser-sync').create();
+var del          = require('del');
+var gulp         = require('gulp');
+var plugins      = require('gulp-load-plugins')();
+
+gulp.task('clean', function(done) {
+    del([
+        './_tmp'
+    ], done);
 });
 
-gulp.task('js-watch', ['js'], function (done) {
+gulp.task('html', ['sass'], function() {
+  var target = gulp.src('./src/index.html');
+
+  var filterJs = plugins.filter(['**/*.js'], {restore: true});
+
+  return target.pipe(plugins.useref())
+            .pipe(filterJs)
+            .pipe(plugins.debug())
+            .pipe(plugins.uglify())
+            .pipe(filterJs.restore)
+            .pipe(gulp.dest('./_tmp/'));
+})
+
+gulp.task('sass', function() {
+  return gulp.src('./src/**/*.scss')
+        .pipe(plugins.debug())
+        .pipe(plugins.sass())
+        .pipe(plugins.autoprefixer('last 1 version'))
+        .pipe(plugins.csso())
+        .pipe(gulp.dest('./_tmp/'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('js', function () {
+    return gulp.src(['./src/**/*.js', '!**/*.min.js'])
+        .pipe(plugins.jshint())
+        .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('js-watch', ['js'], function(done) {
     browserSync.reload();
     done();
 });
 
-gulp.task('serve', ['js'], function() {
+gulp.task('serve', ['html'], function() {
     browserSync.init({
         port: 8080,
         server: {
-            baseDir: "./src/"
+            baseDir: ['./src/', './_tmp']
         }
     });
 
-    gulp.watch("./src/**/*.js", ['js-watch']);
+    gulp.watch('./src/**/*.js', ['js-watch']);
+    gulp.watch('./src/**/*.scss', ['sass']);
+    gulp.watch('./src/**/*.html').on('change', browserSync.reload);
 });
